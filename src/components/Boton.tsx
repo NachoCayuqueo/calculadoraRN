968b6b3491818e3ca05e1843807c2fd82a1770e3
Nacho Cayuqueo <cayuqueoNacho@gmail.com>
2022-04-17 18:40:26 -0300

    styles were added

--- src/components/Boton.tsx
+++ src/components/Boton.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 
 interface Props {
+  ancho?: boolean;
   color?: string;
   texto: string;
 }
 
-export const Boton = ({color = '#2d2d2d', texto}: Props) => {
+export const Boton = ({ancho = false, color = '#2d2d2d', texto}: Props) => {
   return (
-    <View style={{...styles.boton, backgroundColor: color}}>
-      <Text style={styles.botonTexto}>{texto}</Text>
-    </View>
+    <TouchableOpacity>
+      <View
+        style={{
+          ...styles.boton,
+          backgroundColor: color,
+          width: ancho ? 180 : 80,
+        }}>
+        <Text
+          style={{
+            ...styles.botonTexto,
+            color: color === '#9b9b9b' ? 'black' : 'white',
+          }}>
+          {texto}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
